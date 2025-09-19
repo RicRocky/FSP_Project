355dde164ed02ac4182c95ec8e0c3176dc5360fa
Arunika <86771057+RicRocky@users.noreply.github.com>
2025-09-19 12:10:08 +0700

    [Init] Database Koneksi

---
+++ backend/db/Connection.php
@@ -0,0 +1,9 @@
+<?php
+require_once("data.php");
+
+class Connection
+{
+    protected $mysqli;
+    public function __construct() { $this->mysqli = new mysqli(SERVER_NAME, USER_NAME, PASSWORD, DB_NAME);}
+}
+?>
