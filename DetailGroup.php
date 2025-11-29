ef03f54e32c6d896c409b55b794a5dd309b282e9
aileenaverina <167596845+aileenaverina@users.noreply.github.com>
2025-11-29 17:21:56 +0700

    [DetailGrup] On Progress

--- DetailGroup.php
+++ DetailGroup.php
@@ -29,18 +29,58 @@ $res = $group->GetGroupById($_GET['id']);
     foreach ($res as $row) {
         echo "Group name: " . $row["nama"];
     } ?></h1>
-    <h2>Created by <?php foreach ($res as $row) { echo $row["username_pembuat"]; } ?></h2>
-    <p>Type: <?php foreach ($res as $row) { echo $row["jenis"]; } ?></p>
+    <h2>Created by <?php foreach ($res as $row) {
+        echo $row["username_pembuat"];
+    } ?></h2>
+    <p>Type: <?php foreach ($res as $row) {
+        echo $row["jenis"];
+    } ?></p>
     <p>
         Description: <?php
         foreach ($res as $row) {
             echo $row["deskripsi"];
         } ?>
     </p>
-    <h3>Registration code: <?php foreach ($res as $row) { echo $row["kode_pendaftaran"]; } ?></h3>
+    <h3>Registration code: <?php foreach ($res as $row) {
+        echo $row["kode_pendaftaran"];
+    } ?></h3>
+    <table>
+        <thead>
+            <tr>
+                <th>Judul</th>
+                <th>Judul-slug</th>
+                <th>Tanggal</th>
+                <th>Keterangan</th>
+                <th>Jenis</th>
+                <th>Foto</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $stmt = $mysqli->prepare("SELECT * FROM event");
+            $stmt->execute();
+            $res = $stmt->get_result();
+
+            while ($row = $res->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td> " . $row["judul"] . "</td>";
+                echo "<td> " . $row["judul-slug"] . "</td>";
+                echo "<td> " . $row["tanggal"] . "</td>";
+                echo "<td> " . $row["keterangan"] . "</td>";
+                echo "<td> " . $row["jenis"] . "</td>";
+                if (isset($row["poster-extension"])) {
+                    echo "<td><img src='image/" . $row["idevent"] . "." . $rowGambar["poster-extension"] . "' height='75'></td>";
+                } else {
+                    echo "<td>Tidak ada gambar</td>";
+                }
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
     <?php
     if ($_SESSION['role'] == "dosen") {
-        print ('<a href="EditGroup.php?id=' . $_GET['id'] ."&nama=" . $row["nama"] . "&jenis=" . $row["jenis"].'">Edit group</a>');
+        print ('<a href="EditGroup.php?id=' . $_GET['id'] . "&nama=" . $row["nama"] . "&jenis=" . $row["jenis"] . '">Edit group</a>');
     }
     ?>
     <br>
